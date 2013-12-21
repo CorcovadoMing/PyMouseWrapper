057d1a7dac20415a70ad936331e1bca24fe6fc67
CorcovadoMing <rf37535@gmail.com>
2013-12-21 20:33:49 +0800

    update

--- pymousewrapper.py
+++ pymousewrapper.py
@@ -1,4 +1,13 @@
-from Quartz.CoreGraphics import *
+from Quartz.CoreGraphics import CGEventCreateMouseEvent
+from Quartz.CoreGraphics import kCGMouseButtonLeft
+from Quartz.CoreGraphics import kCGHIDEventTap
+from Quartz.CoreGraphics import kCGEventMouseMoved
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseUp
+from Quartz.CoreGraphics import CGEventCreate
+from Quartz.CoreGraphics import CGEventGetLocation
+from Quartz.CoreGraphics import CGEventPost
+import time 
 
 def mouseEvent(type, posx, posy):
     theEvent = CGEventCreateMouseEvent(
@@ -9,17 +18,19 @@ def mouseEvent(type, posx, posy):
     CGEventPost(kCGHIDEventTap, theEvent)
 
 def mouseMove(posx, posy):
+    time.sleep(0.09)
     mouseEvent(kCGEventMouseMoved, posx, posy);
 
-def mouseClick(posx, posy):
+def mouseClickDown(posx, posy):
     mouseEvent(kCGEventLeftMouseDown, posx,posy);
+
+def mouseClickUp(posx, posy):
     mouseEvent(kCGEventLeftMouseUp, posx,posy);
 
-def mouseDrag(start_posx, start_posy, end_posx, end_posy):
-    mouseEvent(kCGEventLeftMouseDown, start_posx, start_posy)
-    mouseMove(end_posx, end_posy)
-    mouseEvent(kCGEventLeftMouseUp, end_posx, end_posy)
-    
+def mouseClick(posx, posy):
+    mouseClickDown(posx, posy)
+    mouseClickUp(posx, posy)
+
 def getLocation():
     ourEvent = CGEventCreate(None)
     currentpos = CGEventGetLocation(ourEvent)
